14e838e197e440fb52124eef72ac957c923b2aa8
Nikolas Haug <nikohaug21@gmail.com>
2021-06-10 12:35:52 -0500

    add all pages and components

---
+++ pages/about.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+
+const About = () => {
+    return ( 
+        <>
+            <Head>
+                <title>Ninjas List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ipsa architecto nihil nostrum corporis laudantium, rerum placeat numquam asperiores officiis quod odio nisi recusandae earum non impedit dolorum? Praesentium, labore.</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A autem molestiae eos dignissimos quo temporibus eum voluptatibus quibusdam quas, accusamus consectetur. Magni quam dicta incidunt blanditiis eaque adipisci optio consequatur?</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe minus accusantium iure beatae dolor, placeat repudiandae quaerat veritatis ad nostrum doloremque explicabo eveniet enim qui asperiores obcaecati quasi officiis nulla.</p>
+            </div>
+        </>
+     );
+}
+ 
+export default About;
